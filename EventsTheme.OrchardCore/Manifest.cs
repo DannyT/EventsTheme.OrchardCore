15c326845e069e3ff88634e5f8dc2e42a5de9ea8
DanMoov2 <dan@moov2.com>
2018-09-26 19:18:47 +0100

    first commit

---
+++ EventsTheme.OrchardCore/Manifest.cs
@@ -0,0 +1,9 @@
+﻿using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "EventsTheme.OrchardCore",
+    Author = "Dan Thomas",
+    Website = "https://danny-t.co.uk",
+    Version = "0.0.1",
+    Description = "Base theme for event websites"
+)]
